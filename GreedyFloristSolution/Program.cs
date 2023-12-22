8b79d6bc84071e22afc2629b4ee91c523531c4b9
Youssef Abdullah <youssef@linked-sys.com>
2023-12-22 20:33:10 +0200

    Greedy Florist Solution

---
+++ GreedyFloristSolution/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var numberOfFriends = Convert.ToInt32(Console.ReadLine());
+        var numberOfFlowers = Convert.ToInt32(Console.ReadLine());
+
+        var flowersPrices = new List<int>();
+        for (int i = 0; i < numberOfFlowers; i++)
+            flowersPrices.Add(Convert.ToInt32(Console.ReadLine()));
+
+        flowersPrices = flowersPrices.OrderByDescending(x => x).ToList();
+        var totalCost = 0;
+        for (int i = 0; i < numberOfFlowers; i++)
+            totalCost += (i / numberOfFriends + 1) * flowersPrices[i];
+
+        Console.WriteLine(totalCost);
+    }
+}
